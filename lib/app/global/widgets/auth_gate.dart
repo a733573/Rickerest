4f64e21da9a4940928fa30875c1fff3580e0eecc
a733573 <a733573@gmail.com>
2022-04-10 11:44:51 +0900

    白紙のローディング画面を追加

--- lib/app/global/widgets/auth_gate.dart
+++ lib/app/global/widgets/auth_gate.dart
@@ -13,29 +13,35 @@ class AuthGate extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       initialData: FirebaseAuth.instance.currentUser,
       builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Sign in'),
-              elevation: 0,
-              backgroundColor: Colors.brown.withOpacity(0.5),
-              automaticallyImplyLeading: false,
-            ),
-            body: SignInScreen(
-              headerBuilder: (context, constraints, _) =>
-                  const SenbeiBayashiImage(),
-              sideBuilder: (context, constraints) => const SenbeiBayashiImage(),
-              providerConfigs: const [
-                GoogleProviderConfiguration(
-                  clientId: '1039744153156-56ibedqkldacjspcgb35nbqaq325d023'
-                      '.apps.googleusercontent.com',
-                ),
-                EmailProviderConfiguration(),
-              ],
-            ),
-          );
+        // ローディング中
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SizedBox();
         }
-        return child;
+        // ログイン済み
+        if (snapshot.hasData) {
+          return child;
+        }
+        // 未ログイン
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Sign in'),
+            elevation: 0,
+            backgroundColor: Colors.brown.withOpacity(0.5),
+            automaticallyImplyLeading: false,
+          ),
+          body: SignInScreen(
+            headerBuilder: (context, constraints, _) =>
+                const SenbeiBayashiImage(),
+            sideBuilder: (context, constraints) => const SenbeiBayashiImage(),
+            providerConfigs: const [
+              GoogleProviderConfiguration(
+                clientId: '1039744153156-56ibedqkldacjspcgb35nbqaq325d023'
+                    '.apps.googleusercontent.com',
+              ),
+              EmailProviderConfiguration(),
+            ],
+          ),
+        );
       },
     );
   }
