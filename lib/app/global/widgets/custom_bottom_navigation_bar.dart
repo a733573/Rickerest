c34214a26bd36067ef56fa661c2409e371b3feb8
a733573 <a733573@gmail.com>
2022-04-14 07:38:54 +0900

    テーマ変更用の外部パッケージ(easy_dynamic_theme)導入

--- lib/app/global/widgets/custom_bottom_navigation_bar.dart
+++ lib/app/global/widgets/custom_bottom_navigation_bar.dart
@@ -1,57 +1,42 @@
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:rickerest/app/core/theme/app_theme.dart';
 import 'package:rickerest/app/routes/app_pages.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
-  const CustomBottomNavigationBar({
-    Key? key,
-    required this.selectedIndex,
-  }) : super(key: key);
+  const CustomBottomNavigationBar({Key? key, required this.selectedIndex})
+      : super(key: key);
 
   final int selectedIndex;
-
-  void _onItemTapped(int index) {
-    {
-      switch (index) {
-        case 0:
-          Get.toNamed(Routes.home);
-          break;
-        case 1:
-          Get.offNamed(Routes.chats);
-          break;
-        case 2:
-          Get.offNamed(Routes.settings);
-          break;
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return ConvexAppBar(
       initialActiveIndex: selectedIndex,
-      onTap: _onItemTapped,
-      backgroundColor: currentThemeData.bottomAppBarColor,
+      onTap: _changeRoute,
+      color: Theme.of(context).primaryColor,
+      activeColor: Theme.of(context).primaryColor,
+      backgroundColor: Theme.of(context).bottomAppBarColor,
+      disableDefaultTabController: false,
       style: TabStyle.react,
       items: const [
         TabItem(
-          icon: Icon(Icons.home_outlined),
+          icon: Icon(Icons.home),
           title: 'Home',
         ),
         TabItem(
-          icon: Icon(Icons.chat_outlined),
+          icon: Icon(Icons.chat),
           title: 'Chats',
         ),
         TabItem(
-          icon: Icon(Icons.settings_outlined),
+          icon: Icon(Icons.settings),
           title: 'Settings',
         ),
       ],
     );
   }
 
-  ThemeData get currentThemeData =>
-      Get.isDarkMode ? AppTheme.dark : AppTheme.light;
+  void _changeRoute(int index) {
+    Get.offNamed([Routes.home, Routes.chats, Routes.settings][index]);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
+import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:rickerest/app/modules/settings/controllers/settings_controller.dart';
 import 'package:rickerest/firebase_options.dart';
 
 import 'app/core/theme/app_theme.dart';
@@ -10,12 +9,8 @@ import 'app/routes/app_pages.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await GetStorage.init();
-  Get.put(SettingsController());
-  runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(EasyDynamicThemeWidget(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -27,8 +22,7 @@ class MyApp extends StatelessWidget {
       title: 'Rickerest',
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
-      themeMode: ThemeMode
-          .values[SettingsController.to.settingsModel.currentThemeModeIndex],
+      themeMode: EasyDynamicTheme.of(context).themeMode!,
       initialRoute: AppPages.initial,
       getPages: AppPages.routes,
       unknownRoute: AppPages.notFound,
