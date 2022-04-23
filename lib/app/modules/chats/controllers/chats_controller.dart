bbdd373afc9a5e5580cb40796f049df39f10af4f
a733573 <a733573@gmail.com>
2022-04-24 05:47:38 +0900

    chatsView:latestMessageの時間表示、字数制限

--- lib/app/modules/chats/controllers/chats_controller.dart
+++ lib/app/modules/chats/controllers/chats_controller.dart
@@ -2,4 +2,19 @@ import 'package:get/get.dart';
 
 class ChatsController extends GetxController {
   static ChatsController get to => Get.find();
+
+  String limitText(String text) {
+    return text.length > 54 ? '${text.substring(0, 54)}...' : text;
+  }
+
+  String relativeTime(DateTime d) {
+    final diff = DateTime.now().difference(d);
+    if (diff.inHours < 24) {
+      return '${d.hour}:${d.minute.toString().padLeft(2, '0')}';
+    } else if (diff.inDays == 1) {
+      return 'yesterday';
+    } else {
+      return '${d.month}/${d.day}';
+    }
+  }
 }
